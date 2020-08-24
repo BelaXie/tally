3c29efd311b9e62d1971de5f645495d0400ab80a
bela <1981253197@qq.com>
2020-08-24 15:59:44 +0800

    使用js完成了类型的切换

---
+++ src/components/Money/Types.vue
@@ -0,0 +1,50 @@
+<template>
+  <ul class="types">
+    <li :class="type==='-'&&'selected'" @click="selectType('-')">支出</li>
+    <li :class="type==='+'&&'selected'" @click="selectType('+')">收入</li>
+  </ul>
+</template>
+<script lang="js">
+export default {
+  data() {
+    return {
+      type:"-", //-表示支出，+表示收入
+    };
+  },
+  methods:{
+    selectType(type){
+      console.log('点击了')
+      if(type!=='-' && type!=='+'){
+        throw new Error('unknown type')
+      }
+      this.type=(this.type==='-'? '+':'-');
+      console.log(this.type)
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+.types {
+  background: #c4c4c4;
+  display: flex;
+  text-align: center;
+  font-size: 24px;
+  > li {
+    width: 50%;
+    height: 64px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    &.selected::after {
+      content: "";
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 4px;
+      background: #333;
+    }
+  }
+}
+</style>
