988f3a42ed22ed80ee842c4e36ea24d0cfb64897
bela <1981253197@qq.com>
2020-10-11 18:38:18 +0800

    deploy

---
+++ precache-manifest.b1e4f97001332c6ced82c4c58d6ec63f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0116adcd220666456a43a603d913b033",
+    "url": "/tally/index.html"
+  },
+  {
+    "revision": "abf43cc2a0ca2d1cda82",
+    "url": "/tally/js/app.3f8a8e6e.js"
+  },
+  {
+    "revision": "10e73bf9ad279f4df8a9",
+    "url": "/tally/js/chunk-vendors.83003d28.js"
+  },
+  {
+    "revision": "11ef934ab77a2f9b18870eb609b61517",
+    "url": "/tally/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tally/robots.txt"
+  }
+]);
